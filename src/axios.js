a921ef798e9f9451b5111aa79625ed78f1994000
marc0000000 <marc@accuhit.net>
2023-03-16 15:42:30 +0800

    換 api 路由

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL:
-    'https://open-ai-3dm-dev-01.openai.azure.com/openai/deployments/test-davinci/completions?api-version=2022-12-01',
+    'https://open-ai-product-dev-01.openai.azure.com/openai/deployments/text-davinci/completions?api-version=2022-12-01',
   headers: {
     'api-key': import.meta.env.VITE_OPEN_AI_KEY
   }
